da88f7397eaa549e86dfa050b4d56d44e34419be
Phillip England <phillip.mark.england@gmail.com>
2024-04-01 20:38:35 -0500

    implemented a common interface for all DocNodes to share. Working on properly sorting doc nodes correctly

--- internal/contentrouter/contentrouter.go
+++ internal/contentrouter/contentrouter.go
@@ -6,26 +6,62 @@ import (
 	"os"
 )
 
-type DocNode struct {
-	Depth        int
-	Parent       string
-	Name         string
-	MarkdownFile *string
+const (
+	DocRoot        = "Root"
+	DocJSONKey     = "docs"
+	JSONConfigPath = "./godocument.config.json"
+)
+
+// each line in the godocument.config.json under the "docs" section is a DocNode
+type DocNode interface {
+	Print()
 }
 
-type DocConfig []*DocNode
+// a slice of DocNodes representing the structured data
+type DocNodes []DocNode
+
+// all DocNodes should implement this type in their struct
+type BaseDocData struct {
+	Depth  int
+	Parent string
+	Name   string
+}
+
+func (b *BaseDocData) GetBaseData() string {
+	return fmt.Sprintf("%d | %s | %s | ", b.Depth, b.Parent, b.Name)
+}
+
+type DocNodeString struct {
+	BaseDocData  *BaseDocData
+	MarkdownFile string
+}
+
+func (b *DocNodeString) Print() {
+	baseData := b.BaseDocData.GetBaseData()
+	fmt.Println(baseData + b.MarkdownFile)
+}
+
+type DocNodeObject struct {
+	BaseDocData *BaseDocData
+	Children    DocNodes
+}
+
+func (b *DocNodeObject) Print() {
+	baseData := b.BaseDocData.GetBaseData()
+	fmt.Println(baseData)
+}
 
 func GenerateRoutes() {
 	uDocs := GetUnstructuredDocs()
-	docs := GetStructuredDocs(uDocs, "Root", DocConfig{}, 0)
+	docs := GetStructuredDocs(uDocs, DocRoot, DocNodes{}, 0)
 	for _, doc := range docs {
-		fmt.Println(doc)
+		doc.Print()
 	}
 }
 
 // GetUnstructuredDocs reads the godocument.config.json file and returns the unstructured data
 func GetUnstructuredDocs() map[string]interface{} {
-	file, err := os.ReadFile("./godocument.config.json") // Ensure the file path and extension are correct
+	file, err := os.ReadFile(JSONConfigPath) // Ensure the file path and extension are correct
 	if err != nil {
 		panic(err)
 	}
@@ -34,37 +70,41 @@ func GetUnstructuredDocs() map[string]interface{} {
 	if err != nil {
 		panic(err)
 	}
-	docs := result["docs"]
+	docs := result[DocJSONKey]
 	return docs.(map[string]interface{})
 }
 
 // GetStructuredDocs takes the unstructured data and returns a structured data
-func GetStructuredDocs(docs map[string]interface{}, parent string, docSlice DocConfig, depth int) DocConfig {
+func GetStructuredDocs(docs map[string]interface{}, parent string, docNodes DocNodes, depth int) DocNodes {
 	for key, value := range docs {
 		switch value := value.(type) {
 		case string:
 			if depth == 0 {
-				parent = "Root"
+				parent = DocRoot
 			}
-			docNode := &DocNode{
-				Depth:        depth,
-				Parent:       parent,
-				Name:         key,
-				MarkdownFile: &value,
+			docNode := &DocNodeString{
+				BaseDocData: &BaseDocData{
+					Depth:  depth,
+					Parent: parent,
+					Name:   key,
+				},
+				MarkdownFile: value,
 			}
-			docSlice = append(docSlice, docNode)
+			docNodes = append(docNodes, docNode)
 		case map[string]interface{}:
-			docNode := &DocNode{
-				Depth:        depth,
-				Parent:       parent,
-				Name:         key,
-				MarkdownFile: nil,
+			docNode := &DocNodeObject{
+				BaseDocData: &BaseDocData{
+					Depth:  depth,
+					Parent: parent,
+					Name:   key,
+				},
+				Children: nil,
 			}
-			docSlice = append(docSlice, docNode)
-			docSlice = GetStructuredDocs(value, key, docSlice, depth+1)
+			docNodes = append(docNodes, docNode)
+			docNodes = GetStructuredDocs(value, key, docNodes, depth+1)
 		default:
 			panic("Invalid type")
 		}
 	}
-	return docSlice
+	return docNodes
 }
