9c33143f9cfbc5be46ae0c890512631e271c20c4
Phillip England <phillip.mark.england@gmail.com>
2024-04-01 20:09:17 -0500

    trying to implement an inteface for DocNodes

--- internal/contentrouter/contentrouter.go
+++ internal/contentrouter/contentrouter.go
@@ -13,40 +13,33 @@ type DocNode struct {
 	MarkdownFile *string
 }
 
+type DocConfig []*DocNode
+
 func GenerateRoutes() {
 	uDocs := GetUnstructuredDocs()
-	docs := GetStructuredDocs(uDocs, "Root", []*DocNode{}, 0)
+	docs := GetStructuredDocs(uDocs, "Root", DocConfig{}, 0)
 	for _, doc := range docs {
 		fmt.Println(doc)
 	}
 }
 
+// GetUnstructuredDocs reads the godocument.config.json file and returns the unstructured data
 func GetUnstructuredDocs() map[string]interface{} {
-
-	// Open the JSON file
 	file, err := os.ReadFile("./godocument.config.json") // Ensure the file path and extension are correct
 	if err != nil {
 		panic(err)
 	}
-
-	// Unmarshal JSON to a map of interfaces
 	var result map[string]interface{}
 	err = json.Unmarshal(file, &result)
 	if err != nil {
 		panic(err)
 	}
-
-	// Get the "docs" key from the map
 	docs := result["docs"]
-	return IToStrMap(docs)
-
+	return docs.(map[string]interface{})
 }
 
-func IToStrMap(i interface{}) map[string]interface{} {
-	return i.(map[string]interface{})
-}
-
-func GetStructuredDocs(docs map[string]interface{}, parent string, docSlice []*DocNode, depth int) []*DocNode {
+// GetStructuredDocs takes the unstructured data and returns a structured data
+func GetStructuredDocs(docs map[string]interface{}, parent string, docSlice DocConfig, depth int) DocConfig {
 	for key, value := range docs {
 		switch value := value.(type) {
 		case string:
