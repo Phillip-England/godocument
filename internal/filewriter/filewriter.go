1d39bcadc603c4e3b1489b18248e9a1778f4c360
Phillip England <phillip.mark.england@gmail.com>
2024-04-18 06:59:12 -0500

    reworked layout using tailwind and css grid

--- internal/filewriter/filewriter.go
+++ internal/filewriter/filewriter.go
@@ -25,10 +25,10 @@ import (
 // GenerateDynamicNavbar generates the dynamic navbar based on ./godocument.config.json
 func GenerateDynamicNavbar(cnf stypes.DocConfig) {
 	html := `
-		<nav id='sitenav' class='flex-col fixed lg:relative z-40 h-screen border-r bg-white w-[80%] lg:w-auto' style="grid-area: sitenav;">
+		<nav id='sitenav' class='fixed top-0 w-[80%] lg:w-auto lg:sticky lg:top-[75px] h-screen border-r z-40 lg:z-0 bg-white' style="grid-area: sitenav;">
 			<div class='sitenav-mobile-header flex flex-row justify-between items-center text-md h-[75px] p-4 border-b lg:hidden'>
 				<div class='sitenav-mobile-header-logo-wrapper flex flex-row items-center justify-between w-[250px]'>
-					<div class="sitenav-mobile-header-logo">
+					<div class="sitenav-mobile-header-logo flex">
 						<img class='logo' src="/static/img/logo.svg" alt="logo" id="logo">
 					</div>
 				</div>
@@ -37,7 +37,7 @@ func GenerateDynamicNavbar(cnf stypes.DocConfig) {
 						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"/>
 					</svg>
 					<svg class="moon-icon cursor-pointer hidden" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-  						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"/>
+						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"/>
 					</svg>
 				</div>
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': mainColor,
+        'main': mainColor,
         'black': black,
       },
       fontFamily: {
