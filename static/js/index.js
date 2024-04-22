570ab06f6684581abb362532e26e01ac2091ea03
Phillip England <phillip.mark.england@gmail.com>
2024-04-22 06:52:20 -0500

    got <mkd-important> and <mkd-warning> working properly

--- static/js/index.js
+++ static/js/index.js
@@ -305,9 +305,8 @@ The utility function eReset(node, eventType, callback) is used to detach and re-
                 this.parentElement.replaceWith(this)
                 let text = this.getAttribute('text')
                 text = replaceBackticksWithCodeTags(text)
-                this.classList.add('custom-element')
                 this.innerHTML = `
-                    <div class='bg-[var(--mkd-important-bg-color)] dark:bg-[var(--dark-mkd-important-bg-color)] p-4 rounded-md border-l-4 border-[var(--mkd-important-border-color)] dark:border-[var(--dark-mkd-important-border-color)] flex flex-col gap-2'>
+                    <div class='bg-[var(--mkd-bg-color)] dark:bg-[var(--dark-mkd-bg-color)] p-4 rounded-md border-l-4 border-[var(--mkd-important-border-color)] dark:border-[var(--dark-mkd-important-border-color)] flex flex-col gap-2'>
                         <span class='flex flex-row item-center gap-2 dark:text-[var(--dark-mkd-important-text-color)] text-[var(--mkd-important-text-color)]'>
                             <span class='flex items-center dark:text-[var(--dark-mkd-important-text-color)] text-[var(--mkd-important-text-color)]'>
                                 <svg class="h-6 w-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -334,14 +333,16 @@ The utility function eReset(node, eventType, callback) is used to detach and re-
                 let text = this.getAttribute('text')
                 text = replaceBackticksWithCodeTags(text)
                 this.innerHTML = `
-                    <div class='bg-[var(--mkd-important-bg-color)] dark:bg-[var(--dark-mkd-important-bg-color)] p-4 rounded-md border-l-4 border-[var(--mkd-important-border-color)] dark:border-[var(--dark-mkd-important-border-color)] flex flex-col gap-2'>
-                        <span class='flex flex-row item-center gap-2 dark:text-[var(--dark-mkd-important-text-color)] text-[var(--mkd-important-text-color)]'>
-                            <span class='flex items-center dark:text-[var(--dark-mkd-important-text-color)] text-[var(--mkd-important-text-color)]'>
-                                <svg class="h-6 w-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                                </svg>
+                    <div class='bg-[var(--mkd-bg-color)] dark:bg-[var(--dark-mkd-bg-color)] p-4 rounded-md border-l-4 border-[var(--mkd-warning-border-color)] dark:border-[var(--dark-mkd-warning-border-color)] flex flex-col gap-2'>
+                        <span class='flex flex-row item-center gap-2 dark:text-[var(--dark-mkd-warning-text-color)] text-[var(--mkd-warning-text-color)]'>
+                            <span class='flex items-center dark:text-[var(--dark-mkd-warning-text-color)] text-[var(--mkd-warning-text-color)]'>
+                            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M12 17a2 2 0 0 1 2 2h-4a2 2 0 0 1 2-2Z"/>
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.815 9H16.5a2 2 0 1 0-1.03-3.707A1.87 1.87 0 0 0 15.5 5 1.992 1.992 0 0 0 12 3.69 1.992 1.992 0 0 0 8.5 5c.002.098.012.196.03.293A2 2 0 1 0 7.5 9h3.388m2.927-.985v3.604M10.228 9v2.574M15 16h.01M9 16h.01m11.962-4.426a1.805 1.805 0 0 1-1.74 1.326 1.893 1.893 0 0 1-1.811-1.326 1.9 1.9 0 0 1-3.621 0 1.8 1.8 0 0 1-1.749 1.326 1.98 1.98 0 0 1-1.87-1.326A1.763 1.763 0 0 1 8.46 12.9a2.035 2.035 0 0 1-1.905-1.326A1.9 1.9 0 0 1 4.74 12.9 1.805 1.805 0 0 1 3 11.574V12a9 9 0 0 0 18 0l-.028-.426Z"/>
+                            </svg>
+                          
                             </span>
-                            <p class='font-bold'>Important</p>                   
+                            <p class='font-bold'>Warning</p>                   
                         </span>
                         <p>${text}</p>
                     </div>
